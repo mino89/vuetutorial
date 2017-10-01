91c0006ab0361665a5905f0a08d48b3b520e4f83
pierdomenico reitano <pierdomenico@lotrek.it>
2017-10-01 02:05:58 +0200

    learning mv!

--- src/main.js
+++ src/main.js
@@ -60,3 +60,14 @@ var app7 = new Vue({
       ]
     }
   })
+
+// data object
+new Vue({
+    data: {
+      a: 1
+    },
+    created: function () {
+      // `this` points to the vm instance
+      console.log('a is: ' + this.a)
+    }
+  })
